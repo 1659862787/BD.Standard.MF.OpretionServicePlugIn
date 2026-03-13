ae41b028a414f7381f8305ec3d9e4985483cfec1
笑颜予妳 <1659862787@qq.com>
2026-03-13 09:37:47 +0800

    1

--- Runs.cs
+++ Runs.cs
@@ -34,7 +34,7 @@ namespace BD.Standard.MF.OpretionServicePlugIn
                 string parameterFormId = schedule.ParameterFormId;
                 
 
-                DynamicObjectCollection dys = DBUtils.ExecuteDynamicObject(ctx,string.Format("select FMASTERID,fnumber from  T_BD_MATERIAL where FUSEORGID=1 and FDOCUMENTSTATUS='C' and  FFORBIDSTATUS='B' and fnumber  not in (select fnumber from  [dbo].[MiFei_logs] where fromid='Material') "));
+                DynamicObjectCollection dys = DBUtils.ExecuteDynamicObject(ctx,string.Format("select FMASTERID,fnumber from  T_BD_MATERIAL where FUSEORGID=1 and FDOCUMENTSTATUS='C' and  FFORBIDSTATUS='A' and fnumber  not in (select fnumber from  [dbo].[MiFei_logs] where fromid='Material') "));
                 foreach (DynamicObject dyn in dys)
                 {
                     IOperationResult operationResult = new OperationResult();
